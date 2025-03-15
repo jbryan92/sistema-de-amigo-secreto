c6e6d4655056fa8258cdffa6851a53d0f5584c23
jbryan92 <jbryangf19@gmail.com>
2025-03-15 18:51:38 -0500

    actualizacion final

--- app.js
+++ app.js
@@ -1,1 +1,60 @@
-console.log("Ejecutando el sistema")
+// variables
+
+let AA = agregarAmigo();
+
+// nombres
+function asignarTextoElemento(elemento, texto){
+ let elementoHTML = document.querySelector(elemento);
+ elementoHTML.innerHTML = texto;
+ return;
+}  
+
+asignarTextoElemento("h1","Tu amigo secreto te espera");
+asignarTextoElemento("h2","coloque nombres de los participantes");
+
+//botones
+
+  //añadir
+
+let ar = [];
+function agregarAmigo() {
+
+    let AA = document.getElementById("amigo").value.trim() ;
+    if (AA !=="") {
+        ar.push(AA);
+
+    console.log(ar);
+} else {
+    console.log("no se puede agregar nombre vacío.");
+}
+
+    limpiarcaja();
+} 
+   
+
+function limpiarcaja() {
+    document.querySelector("#amigo").value = "";
+   } 
+
+
+  //iniciar sorteo
+  
+function sortearAmigo() { 
+    
+if (ar.length === 0) {
+    asignarTextoElemento("h2","acabo el sorteo.");
+   console.log("acabo el sorteo");
+   return;
+}
+
+let tinka = Math.floor(Math.random()*ar.length);
+let seleccionado = ar[tinka];
+
+console.log("seleccionado:" + seleccionado);
+
+ar.splice(tinka,1);
+
+
+
+    asignarTextoElemento("h2",seleccionado);
+ } 
